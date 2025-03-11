206241616efa9ad3d445f061fe1000e3712f9b8c
Priya11223 <bangwalpriyanshu@gmail.com>
2025-03-11 23:34:22 +0545

    files added

--- trade_karo/src/main/java/com/trading/entity/EseHi.java
+++ trade_karo/src/main/java/com/trading/entity/EseHi.java
@@ -1,8 +1,18 @@
 package com.trading.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
 public class EseHi {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    private String purpose = "ESE hi AAYE HAI";
 }
