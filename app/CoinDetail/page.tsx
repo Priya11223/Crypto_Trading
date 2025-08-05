7980d71f6cb3e3b456e7f679fdbb2aeeb8af23d4
Priya11223 <bangwalpriyanshu@gmail.com>
2025-08-05 23:09:36 +0530

    New Ommit

--- app/CoinDetail/page.tsx
+++ app/CoinDetail/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const page = () => {
+  const call = () =>{
+    console.log("Hello");
+  }
   return (
     <div>
       This is the new controller
       {
         <p>Here it is</p>
+      }
+      {
+        <p>Hello there </p>
       }
     </div>
   )
